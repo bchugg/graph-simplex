fc04b6da30f12b0690844fcb0cd0336b542ac38f
bchugg <ben.ih.chugg@gmail.com>
2019-05-07 16:19:36 +0100

    first iteration of laplacian decomposition working

--- graph_simplex.py
+++ graph_simplex.py
@@ -5,19 +5,28 @@ def laplacian_decomposition(G):
 	# Compute the eigendecomposition of the Laplacian of the connected graph G
 	# Return the non-zero eigenvalues and corresponding eigenvectors
 	# Note: G should be a networkx graph
+
+	# Gets eigenvalues and vectors of Laplacian
 	L = nx.laplacian_matrix(G).todense()
-	print(L)
 	w, v = np.linalg.eig(L)
-	print(w)
-	print(v)
-	v[:, range(0,len(w)).pop(w.index(0))] # Remove evec corresponding to eval 0
-	w.remove(0) # Remove eval 0
+	w = list(w)
+
+	# Min eigenvalue is only approx 0 due to imprecision
+	# Find index of this minimum eigenvalue
+	abs_evals = list(map(lambda x: abs(x), w))
+	min_eval_index = abs_evals.index(min(abs_evals))
+	
+	# Remove evec corresponding to eval 0
+	eig_range = list(range(0,len(w)))
+	eig_range.pop(min_eval_index)
+	v = v[:, eig_range] 
+	w.pop(min_eval_index) # Remove eval 0
 
 	return w,v
 
 G = nx.complete_graph(10)
 w,v = laplacian_decomposition(G)
-print(w) 
-print(v)
+print('Eigenvalues:', w) 
+print('Eigevectors:', v)
 
 
